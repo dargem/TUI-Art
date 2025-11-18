6e16ad5f665c3251bd789a2d7801c4cc81598b24
dargem <genericz145@gmail.com>
2025-11-18 16:45:19 +1100

    branch propogator

--- src/main/java/com/example/config/GrowthParams.java
+++ src/main/java/com/example/config/GrowthParams.java
@@ -3,8 +3,8 @@ package com.example.config;
 public record GrowthParams(
         double initialLength,
         double lengthDecay,
+        double widthDecay,
         int maxDepth,
         double angleVariance,
-        int branchesPerNode,
-        long seed
+        int branchesPerNode
 ) {}
--- src/main/java/com/example/model_layer/BranchPropogator.java
+++ src/main/java/com/example/model_layer/BranchPropogator.java
@@ -1,6 +1,7 @@
 package com.example.model_layer;
 
 import com.example.config.GrowthParams;
+import com.example.utils.NumberGenerator;
 
 public class BranchPropogator {
     private final GrowthParams PARAMETERS;
@@ -17,11 +18,11 @@ public class BranchPropogator {
         final double length = branchSection.getLength();
         
         final Point next_location = findNextPoint(branchSection.getLocation(), angle, length);
-        final double next_angle = findNextAngle(angle);
         final double next_length = findNextLength(length);
+        final double next_angle = findNextAngle(angle);
+        final double next_width = findNextWidth(branchSection.getWidth());
 
-
-
+        return new BranchSection(next_location, next_length, next_angle, next_width);
     }
 
     private Point findNextPoint(final Point start_point, final double angle, final double length)
@@ -31,10 +32,21 @@ public class BranchPropogator {
         return new Point(end_x, end_y);
     }
 
+    private double findNextLength(final double length)
+    {
+        final double scale = NumberGenerator.getRandomNumber();
+        return length * (1 - PARAMETERS.lengthDecay()*scale);
+    }
+
     private double findNextAngle(final double angle)
     {
-        // generate a uniform random number between -1, 1
-        final double scale = Math.random() * 2 - 1;
+        final double scale = NumberGenerator.getRandomNumber(-1, 1);
         return angle + scale * PARAMETERS.angleVariance();
+    }
+
+    private double findNextWidth(final double width)
+    {
+        final double scale = NumberGenerator.getRandomNumber();
+        return width *  (1 - PARAMETERS.widthDecay()*scale);
     }
 }
