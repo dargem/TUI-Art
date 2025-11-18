6f862be9f436cd5564b4f7602b52dab11a8d62b7
dargem <genericz145@gmail.com>
2025-11-19 10:06:18 +1100

    trunk done

--- src/main/java/com/example/tree_model/Trunk.java
+++ src/main/java/com/example/tree_model/Trunk.java
@@ -9,7 +9,7 @@ import com.example.utils.Point;
 public class Trunk 
 {
     private final TrunkParams PARAMETERS;
-    private LinkedList<BranchSection> trunk_list;
+    private final LinkedList<BranchSection> trunk_list;
     private final double start_x;
 
     public Trunk(TrunkParams params, Point location)
@@ -30,13 +30,25 @@ public class Trunk
 
     public void extendTrunk()
     {
+        // find parameters for the next branch section
         final BranchSection branch_section = trunk_list.getLast();
+        final Point location = branch_section.getLocation();
         final Point next_point = EndPointFinder.findEnd(
-            branch_section.getLocation(), 
+            location, 
             branch_section.getAngle(), 
             branch_section.getLength()
         );
 
+        final double next_angle = find_angle(location.x());
+
+        // add the next branch section to the top
+        trunk_list.add(
+            new BranchSection(
+                next_point, 
+                PARAMETERS.section_length(), 
+                next_angle, PARAMETERS.width()
+            )
+        );
     }
 
     private double find_angle(double current_x)
@@ -44,13 +56,8 @@ public class Trunk
         // this doesn't care about prior states, can consider doing that in the future
         // e.g. if further left then start, then positive offset
         final double needed_realignment = start_x - current_x;
-        final double scale = NumberGenerator.getRandomNumber() + needed_realignment * PARAMETERS.centralness();
+        final double needed_realignment_angle = Math.asin(needed_realignment / PARAMETERS.section_length());
+        final double scale = NumberGenerator.getRandomNumber() + needed_realignment_angle * PARAMETERS.centralness();
         return scale * PARAMETERS.angle_variance();
     }
-
-
-
-
-
-
 }
