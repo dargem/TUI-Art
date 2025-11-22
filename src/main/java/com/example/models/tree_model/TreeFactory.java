e1a3571a4f495198eea521f34b703c025a23d4a3
dargem <genericz145@gmail.com>
2025-11-23 08:31:47 +1100

    basic tree map made in factory

--- src/main/java/com/example/models/tree_model/TreeFactory.java
+++ src/main/java/com/example/models/tree_model/TreeFactory.java
@@ -1,9 +1,14 @@
 package com.example.models.tree_model;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
+
 import com.example.config.BranchParams;
 import com.example.config.TrunkParams;
 import com.example.models.ModelFactory;
 import com.example.representations.Coord;
+import com.example.utils.Colour;
 import com.example.utils.NumberGenerator;
 import com.example.rendering.LineBasedRenderable;
 import com.example.rendering.TileProvider;
@@ -17,12 +22,42 @@ public class TreeFactory implements ModelFactory{
     private static final double GROUND_LEVEL = 0;
     // Potentially have it load startup renders from a config file later
     private static final int NUM_STARTUP_RENDERS = 0;
-    private final TileProvider trunk_tile_provider = new TileProvider(10, 11, , null);
+
+    private final TileProvider trunk_tile_provider;
 
     public TreeFactory(BranchParams branch_params, TrunkParams trunk_params)
     {
         this.branch_params = branch_params;
         this.trunk_params = trunk_params;
+        final TreeMap<Double, ArrayList<Character>> tree_char_map = new TreeMap<>();
+
+        tree_char_map.put(
+            0.0,
+            new ArrayList<>(List.of('|', '[', ']', 'I'))
+        );
+
+        tree_char_map.put(
+            Math.PI/8,
+            new ArrayList<>(List.of('/', '[', 'P'))
+        );
+
+        tree_char_map.put(
+            -Math.PI/8,
+            new ArrayList<>(List.of('\\', ']', 'J')) // only one backslash its just escape
+        );
+
+        tree_char_map.put(
+            Math.PI/2,
+            new ArrayList<>(List.of('-', '_'))
+        );
+
+        trunk_tile_provider = new TileProvider(
+            0,
+            1,
+            Colour.BROWN_LIGHT,
+            tree_char_map
+        );
+
     }
 
     @Override
