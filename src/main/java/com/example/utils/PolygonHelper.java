03c4718bfae0f7fa964e39f777d03cca58d23eb1
dargem <genericz145@gmail.com>
2025-12-03 22:55:54 +1100

    ear clipping almost implemented

--- src/main/java/com/example/utils/PolygonHelper.java
+++ src/main/java/com/example/utils/PolygonHelper.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Collections;
 
 import com.example.representations.Coord;
-
+import com.example.representations.Vector2d;
 public final class PolygonHelper
 {
     // private constructor to stop instantiation
@@ -13,6 +13,8 @@ public final class PolygonHelper
     /**
      * Decomposes a simple polygon into triangles
      * Returns an array, of an array of 3 Coord objects
+     * Uses an ear clipping implementation
+     * https://www.youtube.com/watch?v=hTJFcHutls8&list=PLSlpr6o9vURx4vjomFuwrFhvhV1nhJ_Jc&index=1
      * @param vertices vertices of the polygon (in order)
      * @return An array of array of coords, in form Coord[n][3];
      */
@@ -52,18 +54,61 @@ public final class PolygonHelper
             for (int i = 0; i < index_list.size(); i++)
             {
                 // getItem manages wrap around
-                int a = index_list.get(i);
-                int b = ArrayUtils.getItem(index_list, i - 1);
-                int c = ArrayUtils.getItem(index_list, i + 1);
+                final int a = index_list.get(i);
+                final int b = ArrayUtils.getItem(index_list, i - 1);
+                final int c = ArrayUtils.getItem(index_list, i + 1);
 
-                Coord coord_a = vertices[a];
-                Coord coord_b = vertices[b];
-                Coord coord_c = vertices[c];
+                final Coord coord_a = vertices[a];
+                final Coord coord_b = vertices[b];
+                final Coord coord_c = vertices[c];
+
+                final Vector2d va_to_vb = new Vector2d(coord_a, coord_b);
+                final Vector2d va_to_vc = new Vector2d(coord_a, coord_c);
+
+                // if the cross product is negative, it is reflexive
+                // > PI/2 So a triangle can't be made here
+                if (VectorMath.crossProduct(va_to_vc, va_to_vb) < 0)
+                {
+                    continue;
+                }
+
+                // so its convex vertex, therefore a triangle can be made
+                // Assuming the proposed one isn't containing a vertex
+                boolean is_ear = true;
+                for (int j = 0; j < index_list.size(); ++j)
+                {
+                    final int indice = index_list.get(j);
+                    if (indice == a || indice == b || indice == c)
+                    {
+                        continue;
+                    }
+                    
+                    if (PolygonHelper.isPointInTriangle(coord_a, coord_b, coord_c, vertices[indice]))
+                    {
+                        is_ear = false;
+                        break;
+                    }
+
+                }
+
+                if (is_ear)
+                {
+                    // this is an ear so a triangle can be made
+                    triangles[triangle_count++] = new Coord[]
+                    {
+                        vertices[b],
+                        vertices[a],
+                        vertices[c]
+                    };
+
+                    index_list.remove(i);
+                    break;
+                }
             }
         }
 
         // now add the final triangle
-        triangles[triangles.length - 1] = new Coord[]
+        triangles[triangle_count] = new Coord[]
         {
             vertices[index_list.get(0)],
             vertices[index_list.get(1)],
@@ -101,5 +146,21 @@ public final class PolygonHelper
     private static WindingOrder findWindingOrder(Coord[] vertices)
     {
         throw new UnsupportedOperationException("not implemented");
+    }
+
+    private static boolean isPointInTriangle(Coord a, Coord b, Coord c, Coord p)
+    {
+        final Vector2d va_to_vb = new Vector2d(a, b);
+        final Vector2d vb_to_vc = new Vector2d(b, c);
+        final Vector2d vc_to_va = new Vector2d(c, a);
+
+        final Vector2d va_to_vp = new Vector2d(a, p);
+        final Vector2d vb_to_vp = new Vector2d(b, p);
+        final Vector2d vc_to_vp = new Vector2d(c, p);
+
+        // returns false if any are > 0, if they're > 0 its not in the triangle
+        return !(VectorMath.crossProduct(va_to_vb, va_to_vp) > 0
+                || VectorMath.crossProduct(vb_to_vc, vb_to_vp) > 0
+                || VectorMath.crossProduct(vc_to_va, vc_to_vp) > 0);
     }
 }
