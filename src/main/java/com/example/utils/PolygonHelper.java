e80d2b42a6c554d9b49f14e81e379e6ac1096628
dargem <genericz145@gmail.com>
2025-12-04 20:07:46 +1100

    polygon helper done hopefully

--- src/main/java/com/example/utils/PolygonHelper.java
+++ src/main/java/com/example/utils/PolygonHelper.java
@@ -2,6 +2,7 @@ package com.example.utils;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 
 import com.example.representations.Coord;
 import com.example.representations.Vector2d;
@@ -130,7 +131,99 @@ public final class PolygonHelper
      */
     private static boolean isSimplePolygon(Coord[] vertices)
     {
-        throw new UnsupportedOperationException("isSimplePolygon not implemented");
+        // potentially implement bentley ottman in the future
+        // for now just a lazy loop through
+        int n = vertices.length;
+        HashSet<Integer> neighbours = new HashSet<>(Arrays.asList(0, 1, n-1));
+
+        for (int i = 0; i < n; ++i)
+        {
+            Coord a = vertices[i];
+            Coord b = IterationUtils.getItem(vertices, i + 1);
+
+            for (int j = 0; i < n; ++i)
+            {
+                Coord c = vertices[j];
+                Coord d = IterationUtils.getItem(vertices, i + 1);
+
+                // Skip neighboring edges that share endpoints
+                if (neighbours.contains(Math.abs( i - j)))
+                {
+                    continue;
+                }
+
+                if (PolygonHelper.isIntersectingSegment(a,b,c,d))
+                {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+
+    /**
+     * 2d cross product using p_to_q with r
+     * A positive result means r is counterclockwise
+     * A negative result means r is clockwise
+     * A zero means r is collinear with ->q
+     * @param p coordinate line start
+     * @param q coordinate line end
+     * @param r vector
+     * @return the cross product if p_to_q and r
+     */
+    private static double orient(Coord p, Coord q, Coord r)
+    {
+        return (q.x() - p.x())*(r.y() - p.y()) - (q.y() - p.y())*(r.x() - p.x());
+    }
+
+    /**
+     * Checks whether a point is on the segment
+     * Assumes the inputted point is already colinear with p->q
+     * @param p start of line coord
+     * @param q end of line coord
+     * @param r 
+     * @return
+     */
+    private static boolean isOnSegment(Coord p, Coord q, Coord r)
+    {
+        return (
+            Math.min(p.x(), r.x()) <= q.x() &&
+            Math.max(p.x(), r.x()) >= q.x() &&
+            Math.min(p.y(), r.y()) <= q.y() &&
+            Math.max(p.y(), r.y()) >= q.y()
+        );
+    }
+
+    /**
+     * Checks whether two segments are intersecting
+     * @param a start of 1st line
+     * @param b end of 1st line
+     * @param c start of 2nd line
+     * @param d end of 2nd line
+     * @return whether line 1 and 2 are intersecting
+     */
+    private static boolean isIntersectingSegment(Coord a, Coord b, Coord c, Coord d)
+    {
+        // https://www.youtube.com/watch?v=bbTqI0oqL5U
+        double o1 = PolygonHelper.orient(a, b, c);
+        double o2 = PolygonHelper.orient(a, b, d);
+        double o3 = PolygonHelper.orient(c, d, a);
+        double o4 = PolygonHelper.orient(c, d, b);
+
+        // effectively an xor gate for signs with (a, b) and (c, d)
+        // if c and d are pos/neg for a->b and a, b are pos/neg for c-> d
+        // then they must be intersecting
+        if (o1*o2 < 0 && o3 * o4 < 0)
+        {
+            return true;
+        }
+
+        // handles special cases where a point is lying on the line
+        if (o1 == 0 && PolygonHelper.isOnSegment(a, c, b)) return true;
+        if (o2 == 0 && PolygonHelper.isOnSegment(a, d, b)) return true;
+        if (o3 == 0 && PolygonHelper.isOnSegment(c, a, d)) return true;
+        return o4 == 0 && PolygonHelper.isOnSegment(c, b, d);
     }
 
     /**
