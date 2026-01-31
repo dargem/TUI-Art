11f7c8f65794f6a4f39aaa70be6a7e86e85a3936
dargem <genericz145@gmail.com>
2026-01-31 13:45:59 +1100

    terminal status test

---
+++ cpp/tests/tui/terminal_status_test.cpp
@@ -0,0 +1,53 @@
+#include <gtest/gtest.h>
+#include <cstddef>
+#include "tui/terminal_status.hpp"
+
+#if defined(__linux__) || defined(__unix__)
+#include <unistd.h>
+#elif defined(_WIN32) || defined(_WIN64)
+#include <io.h>
+#endif
+
+using tui::TerminalDimension;
+using tui::TerminalStatus;
+
+static bool is_stdout_tty()
+{
+#if defined(__linux__) || defined(__unix__)
+    return ::isatty(STDOUT_FILENO);
+#elif defined(_WIN32) || defined(_WIN64)
+    return ::_isatty(_fileno(stdout));
+#else
+    return false;
+#endif
+}
+
+TEST(TerminalStatus, TerminalDimensionAccessible)
+{
+    if (!is_stdout_tty())
+    {
+        GTEST_SKIP() << "stdout is not a TTY; terminal size query is expected to fail";
+    }
+    TerminalStatus &terminalStatus{TerminalStatus::getInstance()};
+
+    EXPECT_NO_THROW(TerminalDimension terminalDimension{terminalStatus.getTerminalDimension()}) << "Querying terminal dimension throws an error";
+}
+
+TEST(TerminalStatus, TerminalDimensionSizesLogical)
+{
+    constexpr static size_t MAX_WIDTH{1000};
+    constexpr static size_t MAX_HEIGHT{1000};
+
+    if (!is_stdout_tty())
+    {
+        GTEST_SKIP() << "stdout is not a TTY; terminal size query is expected to fail";
+    }
+
+    TerminalStatus &terminalStatus{TerminalStatus::getInstance()};
+
+    TerminalDimension terminalDimension{terminalStatus.getTerminalDimension()};
+
+    // if its larger than these its probably outputting some nonsense
+    EXPECT_LE(terminalDimension.x, MAX_WIDTH);
+    EXPECT_LE(terminalDimension.y, MAX_HEIGHT);
+}
