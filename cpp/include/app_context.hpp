a1ab008a27d83803a0e60d9d171e359e263df79b
dargem <genericz145@gmail.com>
2026-02-19 21:46:38 +1100

    add logger into app context

--- cpp/include/app_context.hpp
+++ cpp/include/app_context.hpp
@@ -2,22 +2,42 @@
 
 #include "tui/terminal_status.hpp"
 #include "tui/ansi/ansi_printer.hpp"
+#include "utils/logger.hpp"
 
 using tui::TerminalStatus;
 using tui::ansi::Printer;
+using utils::Logger;
+using utils::LogLevel;
 
+template <LogLevel logLevel = LogLevel::INFO>
 class AppContext
 {
 public:
-    AppContext();
+    AppContext()
+        : terminalStatus{}, printer{terminalStatus}, logger{}
+    {
+    }
 
     [[nodiscard]]
-    TerminalStatus &getTerminalStatus();
+    TerminalStatus &getTerminalStatus()
+    {
+        return terminalStatus;
+    }
 
     [[nodiscard]]
-    Printer &getPrinter();
+    Printer &getPrinter()
+    {
+        return printer;
+    }
+
+    [[nodiscard]]
+    Logger<logLevel> &getLogger()
+    {
+        return logger;
+    }
 
 private:
     TerminalStatus terminalStatus;
     Printer printer;
+    Logger<logLevel> logger;
 };
