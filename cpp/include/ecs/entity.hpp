79d6a6b02e1a38c6ba1bc4870f3ded388a2422d4
dargem <genericz145@gmail.com>
2026-03-12 23:09:15 +1100

    more stuff

--- cpp/include/ecs/entity.hpp
+++ cpp/include/ecs/entity.hpp
@@ -1,7 +1,10 @@
 #pragma once
 
+#include <tuple>
+
 #include "cstdint"
 #include "ecs/components.hpp"
+#include "ecs/concepts.hpp"
 #include "utils/sparse_set.hpp"
 
 namespace ECS {
@@ -11,6 +14,18 @@ template <Component... Cs>
 struct Entity {
     utils::ID index;     // used to lookup entity in entityMap
     uint32_t version{};  // incremented when an entity is destroyed
+
+    template <Component C>
+        requires OneOf<C, Cs>
+    C get() {
+        // some holder logic
+    }
+
+    template <Component... Csearch>
+        requires UniqueTypes<Csearch> && Bounded<Csearch, Cs>
+    std::tuple<CSearch...> getComponents() {
+        // get a tuple of its values
+    }
 };
 
 /*
