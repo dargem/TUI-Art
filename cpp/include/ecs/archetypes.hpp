99beb3d1ef5a4685e800e8abd285426404879954
dargem <genericz145@gmail.com>
2026-03-20 23:45:19 +1100

    proper erase logic maybe...

--- cpp/include/ecs/archetypes.hpp
+++ cpp/include/ecs/archetypes.hpp
@@ -22,35 +22,36 @@ class ArchetypeTable {
    public:
     // Add the components of an entity into the archetype table
     ID add() {
+        // Emplace each vector with a new component
         auto emplaceBack = [](auto& vec) { vec.emplace_back(); };
-
         std::apply([&](auto&... vecs) { (emplaceBack(vecs), ...); }, data);
-        return IDIndices[getDataSize() - 1];  // - 1 as we've just emplaced the entity
+
+        return metadata[getDataSize() - 1].rid;
     }
 
     // Remove a component by index, does a swap and pop
-    void remove(ID entityID) {
-        assert(entityID < dataIndices.size());
+    void erase(ID entityID) {
+        assert(entityID < dataIndices.size() && "Out of bounds index");
 
+        // Get relevant info
+        const ID dataID = dataIndices[entityID];
+        const ID lastDataID = getDataSize() - 1;
+        const ID lastID = metadata[lastDataID].rid;
+        // Update validity ID
+        ++metadata[dataID].validityID;
+
+        // Overwrite the object with object at the end, then pop it
         auto swapPop = [](auto& vec, size_t index) {
             assert(index < vec.size());
             vec[index] = std::move(vec.back());
             vec.pop_back();
         };
 
-        size_t dataIndex = dataIndices[entityID];
         // For each component (each nested in a vector) pop and swap it with the last component
-        std::apply([&, dataIndex](auto&... vecs) { (swapPop(vecs, dataIndex), ...); }, data);
-
-        // Swap around the IDIndices of the back and [dataIndices] to keep consistency
-        ID backIDIndex = IDIndices.back();
-        IDIndices.back() = IDIndices[dataIndex];
-        IDIndices[dataIndex] = backIDIndex;
-
-        // Swap around the dataIndices values for consistency
-        size_t swapDataIndex = dataIndices[IDIndices.back()];
-        dataIndices[IDIndices.back()] = dataIndices.back();
-        dataIndices.back() = swapDataIndex;
+        std::apply([&, dataID](auto&... vecs) { (swapPop(vecs, dataID), ...); }, data);
+        // Then realign the data indices and metadata
+        std::swap(metadata[dataID], metadata[lastDataID]);
+        std::swap(dataIndices[entityID], dataIndices[lastID]);
     }
 
     // get an unordered vector of a specific component type
@@ -70,16 +71,21 @@ class ArchetypeTable {
    private:
     inline size_t getDataSize() { return std::get<0>(data).size(); }
 
+    struct Metadata {
+        // The reverse ID allowing retrieval of ID of the object from data vector
+        ID rid{};
+        // An identifier that is changed when object is erased, ensures a handle is still valid
+        ID validityID{};
+    };
+
     // sparse set keying ID's to the dense vectors's index (component data)
-    std::vector<size_t> dataIndices;
+    std::vector<ID> dataIndices;
 
     // maps an index in data back to the id
-    std::vector<ID> IDIndices;
+    std::vector<Metadata> metadata;
 
     // tuple of dense vectors, where each vector holds a component type, SOA style
     std::tuple<std::vector<Cs>...> data;
 };
-
-struct AccessManager {};
 
 }  // namespace ECS
