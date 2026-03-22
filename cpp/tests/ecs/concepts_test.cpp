8ed3bc994025a9ebb1b6255b76bd5f9714182868
dargem <genericz145@gmail.com>
2026-03-22 11:26:41 +1100

    more tests

--- cpp/tests/ecs/concepts_test.cpp
+++ cpp/tests/ecs/concepts_test.cpp
@@ -18,8 +18,19 @@ TEST(ECSConceptTests, UniqueTypesFilters) {
 TEST(ECSConceptTests, OneOfFilters) {
     EXPECT_TRUE((OneOf<int, bool, double, long, int, float>));
     EXPECT_TRUE((OneOf<float, float, double>));
+    EXPECT_FALSE((OneOf<float, int, double>));
 }
 
+TEST(ECSConceptTests, OneOfPackFilters) {
+    // Can also be used with more explicit type pack syntax
+    EXPECT_TRUE((OneOfPack<int, TypePack<bool, double, long, int, float>>));
+    EXPECT_TRUE((OneOfPack<float, TypePack<float, double>>));
+    EXPECT_FALSE((OneOfPack<short, TypePack<float, double>>));
+    EXPECT_TRUE((OneOfPack<short, TypePack<short>>));
+    EXPECT_FALSE((OneOfPack<short, short>)) << "Should fail if you forget to use TypePack syntax";
+}
+
+// Helper class for a "realistic" example
 template <typename... Ts>
 class Foo {
    public:
@@ -32,6 +43,8 @@ class Foo {
 TEST(ECSConceptTests, SameSize) {
     Foo<int, double, long> foo;
     EXPECT_TRUE((foo.sameSize<double, double, float>()));
+    EXPECT_FALSE((foo.sameSize<float>()));
+
     EXPECT_TRUE((SameSizePacks<TypePack<float, bool, double>, TypePack<double, double, double>>));
     EXPECT_FALSE((SameSizePacks<double, double>))
         << "Should be falsy if accidentally not providing a typepack for argument";
