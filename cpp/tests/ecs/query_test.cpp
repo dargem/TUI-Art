db16adf7b6d4534bf9d43ec18b80eb0f121888cf
dargem <genericz145@gmail.com>
2026-03-30 10:18:35 +1100

    deduces argument test

--- cpp/tests/ecs/query_test.cpp
+++ cpp/tests/ecs/query_test.cpp
@@ -1,6 +1,7 @@
 #include "ecs/query.hpp"
 
 #include <functional>
+#include <tuple>
 
 #include "ecs/components.hpp"
 #include "gtest/gtest.h"
@@ -14,8 +15,13 @@ void testProcess(A, B){};
 
 TEST(QueryTests, TemplateArgumentDeduction) {
     std::function<void(A, B)> function = testProcess;
-    Query query{function};
-    // Query query(testProcess);
+    Query query1{function};
+    Query query2(testProcess);
+    Query query3{[](A componentA, B componentB) {}};
+
+    EXPECT_TRUE((std::same_as<decltype(query1)::ArgsTuple, std::tuple<A, B>>));
+    EXPECT_TRUE((std::same_as<decltype(query2)::ArgsTuple, std::tuple<A, B>>));
+    EXPECT_TRUE((std::same_as<decltype(query3)::ArgsTuple, std::tuple<A, B>>));
 }
 
 }  // namespace ECS
