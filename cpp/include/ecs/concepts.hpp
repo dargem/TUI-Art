2465bfc35f431223f8eab07f0e2e26d57e2d50d5
dargem <genericz145@gmail.com>
2026-04-09 12:44:59 +1000

    fix top/low level const stuff

--- cpp/include/ecs/concepts.hpp
+++ cpp/include/ecs/concepts.hpp
@@ -97,9 +97,14 @@ concept IsPermutationPacks = TypePackType<PackA> && TypePackType<PackB> &&
 }  // namespace ECS
 
 // If its a const reference or just a copy its just a read
+// std::is_const_v would not work for const int&
+// this is because its a non const reference, pointing to a const object
+// aka it is low level const, but not high level const
+// is_const_v only checks high level const
 template <typename T>
-concept IsRead = std::is_const_v<T> || (!std::is_reference_v<T> && !std::is_rvalue_reference_v<T>);
+concept IsRead = std::is_const_v<std::remove_reference_t<T>> ||
+                 (!std::is_reference_v<T> && !std::is_rvalue_reference_v<T>);
 
 // A non const reference is a write
 template <typename T>
-concept IsWrite = std::is_lvalue_reference_v<T> && !std::is_const_v<T>;
+concept IsWrite = std::is_lvalue_reference_v<T> && !std::is_const_v<std::remove_reference_t<T>>;
